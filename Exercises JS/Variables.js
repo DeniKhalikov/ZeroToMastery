0f99b9d8bbc78aa34129a162f5525263232785e5
Deni-Khalikov <deni.khalikov@gmail.com>
2020-09-22 16:00:02 +0200

    Basic JS exercises on variables and basic calculator

---
+++ Exercises JS/Variables.js
@@ -0,0 +1,22 @@
+// let firstName = "Deni";
+// let lastName = "Khalikov";
+// let answer = firstName + " " + lastName;
+//
+// console.log(answer);
+
+// let a = 34;
+// let b = 21;
+// a = 2 ;
+// a + b;
+//
+// // result is 23 because a is defined later on again
+//
+// let c; // will be undefined
+
+// CALCULATOR
+
+//
+// let firstNumber = prompt("put your first number: ");
+// let secondNumber = prompt("put your first number: ");
+// let answer = Number(firstNumber) + Number(secondNumber);
+// alert = ("the answer is " + answer);
